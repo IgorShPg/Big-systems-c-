d8522eb7614ef897d0dd4eca9066cf852418f1a0
IgorSH <igor@MacBook-Pro-IgorSH.local>
2024-11-12 22:06:54 +0300

    fix

--- 5/same.cpp
+++ 5/same.cpp
@@ -1,21 +1,26 @@
-#include <iostream>
 #include <type_traits>
+#include <cstdint>
 
-template <typename...>
-struct are_same : std::true_type{};
+template <typename... Ts>
+struct are_same;
 
-template <typename T, typename U, typename... Args>
-struct are_same<T, U, Args...> : std::false_type{};
+template <>
+struct are_same<> : std::true_type {};
 
-template <typename T, typename... Args>
-struct are_same<T, T, Args...> : are_same<T, Args...>{};
+template <typename T, typename U>
+struct is_same_helper : std::is_same<std::remove_cv_t<std::remove_reference_t<T>>, std::remove_cv_t<std::remove_reference_t<U>>> {};
 
-template <typename... Args>
-constexpr bool are_same_v = are_same<Args...>::value;
+
+template <typename T, typename... Ts>
+struct are_same<T, Ts...> : std::conjunction<is_same_helper<T, Ts>...> {};
+
+template <typename... Ts>
+constexpr bool are_same_v = are_same<Ts...>::value;
 
 int main()
 {
     static_assert(are_same_v<int, int32_t, signed int>, "compile assert");
     static_assert(!are_same_v<int, int32_t, unsigned int>, "compile assert");
+    static_assert(are_same_v<int&, const int32_t, volatile signed int&&>, "compile assert");
     return 0;
 }
